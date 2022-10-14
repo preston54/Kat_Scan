551891c2a0844edc0f6f53573fb25d18e4bc7c10
skokilla <1258963o@gmail.com>
2022-10-14 18:25:16 -0500

    basic setup
    Got the basics done and ready to take off coding

---
+++ code/pages/student_profile.jsx
@@ -0,0 +1,23 @@
+const StudentProfile  = () =>
+{
+
+    return ( 
+            <div className="fullpage">
+        
+                  <><h1>  Student Home Page</h1>
+        
+                    <div className="box"> Test words  
+                    <br></br>                 
+                    Student
+                    <br></br>
+
+
+                 </div>
+                 </>
+        
+            </div>
+
+        )
+}
+
+export default StudentProfile;
