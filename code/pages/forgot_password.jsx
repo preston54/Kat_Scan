d0539fe0b93594e3609336ffebc975f228eee604
Abbigale Crump <93808194+ALCrump@users.noreply.github.com>
2022-10-30 14:22:29 -0500

    First Commit

---
+++ code/pages/forgot_password.jsx
@@ -0,0 +1,40 @@
+const forgot_password = () => {
+
+    return(
+
+        <div class="passwordContainer">
+            <div class="App_Name">
+            <img src="https://www.shsu.edu/dept/marketing/logos/SHSU-RGB_Orange%20Box.png" alt="SHSU"></img> 
+                <h2>
+                    Kat Scan
+                </h2>
+            </div>
+            <form class="passwordForm" id="login">
+            <br></br>
+                <div class="header">
+                    <h2>Forgot your password</h2>
+                </div>
+                <br></br>
+                <p>Please enter the SHSU email address you'd like your password reset information sent to</p>
+                <br></br>
+                <div class="passwordForm-control">
+                    <label class="emailInstructionLabel">SHSU email address:</label>
+                    <input class="forgotEmailInput" type="text" autofocus placeholder="abc123@shsu.edu" id="email"/>
+                </div>
+                <br></br>
+                <br></br>
+
+                <button class="passwordForm_Button" type="submit">SUBMIT</button>
+                <br></br>
+                <br></br>
+                
+                <p class="passwordForm__text">
+                    <a href="login_page" class="form__link">Return to login page</a>
+                </p>
+
+            </form>
+        </div>
+    )
+}
+
+export default forgot_password;
---
+++ code/pages/login_page.jsx
@@ -0,0 +1,42 @@
+const login_page = () => {
+
+    return(
+
+        <div class="container">
+            <div class="App_Name">
+            <img src="https://www.shsu.edu/dept/marketing/logos/SHSU-RGB_Orange%20Box.png" alt="SHSU"></img> 
+                <h2 class="KatScan">
+                    Kat Scan
+                </h2>
+                
+            </div>
+            <form class="form" id="login">
+                <div class="header">
+                    <h2>LOGIN</h2>
+                </div>
+                <div class="form-control">
+                    <label class="emailLabel">SHSU Email:</label>
+                    <input class="emailInput" type="text" autofocus placeholder="abc123@shsu.edu" id="email"/>
+                </div>
+                <br></br>
+                <div class="form-control">
+                    <label class="passwordLabel">   Password:</label>
+                    <input class="passwordInput" type="text" autofocus placeholder="Password" id="username"/>
+                </div>
+                <br></br>
+
+                <button class="form__button" type="submit">SUBMIT</button>
+                <br></br>
+
+                <p class="form__text">
+                    <a href="forgot_password" class="form__link">Forgot your password?</a>
+                </p>
+                <p class="form__text">
+                    <a class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create account</a>
+                </p>
+            </form>
+        </div>
+    )
+}
+
+export default login_page;
