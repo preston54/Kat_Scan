5d608ea001e2bfafcf35a6856d479aaf92dc7c6a
preston54 <81720648+preston54@users.noreply.github.com>
2022-11-16 09:01:46 -0600

    class routing

---
+++ code/pages/class/[classname].js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Router from 'next/router';
+
+
+export default function Home({data}) {
+
+    const router = useRouter();
+    // const [uname, setUname] = useState("");
+    // const { classname } = router.query;
+
+    const className = router.query.classname;
+
+    
+
+    // useEffect(() => {
+    //     data?.map(function (item){
+    //         if(item.UserName == user){
+    //             setUname(item.FirstName);
+    //             console.log(item.FirstName);
+    //         }
+    //     });
+
+    // }), [router.query.classname, router.isReady];
+
+     
+
+    const submitLog = async (event) => {
+
+
+      const email = event.target.email.value;
+      const pass = event.target.password.value;
+      const uname = "";
+      event.preventDefault();
+      const apiUrlEndpoint = 'http://localhost:3000/api/logindata-lib';
+          const postData = {
+      
+              method: "POST",
+              headers: {"Content-Type": "application/json"},
+              body: JSON.stringify({
+              Email: email,
+              Password: pass,
+              }),
+      
+          };
+          const response = await fetch(apiUrlEndpoint, postData);
+          
+          if (response.status == 200){
+              const responsedata = await response.json();
+              console.log(responsedata);
+              uname = responsedata[0].UserName;
+              Router.push({
+                  pathname: 'http://localhost:3000/class/markattendance',
+                  query: {
+                    className
+                  },
+              });
+          }
+          else{
+
+              alert("User not found, Please check your email or password");
+          }
+  }
+
+  return (
+    <div className="container">
+    <div className="App_Name">
+    <img src="https://www.shsu.edu/dept/marketing/logos/SHSU-RGB_Orange%20Box.png" alt="SHSU"></img> 
+        <h2 className="KatScan">
+            Kat Scan
+        </h2>
+        
+    </div>
+    <form className="form" onSubmit = {submitLog} id="login">
+        <div className="header">
+            <h2>LOGIN</h2>
+        </div>
+        <div className="form-control">
+            <label className="emailLabel">SHSU Email:</label>
+            <input className="emailInput" type="text"  placeholder="abc123@shsu.edu" id="email"/>
+        </div>
+        <br></br>
+        <div className="form-control">
+            <label className="passwordLabel">   Password:</label>
+            <input className="passwordInput" type="text"  placeholder="Password" id="password"/>
+        </div>
+        <br></br>
+
+        <button className="form__button" type="submit">SUBMIT</button>
+        <br></br>
+
+        <p className="form__text">
+            <a href="forgot_password" className="form__link">Forgot your password?</a>
+        </p>
+        <p className="form__text">
+            <a className="form__link" href="createaccount" id="linkCreateAccount">Don't have an account? Create account</a>
+        </p>
+    </form>
+</div>
+  )
+}
+
+// export async function getServerSideProps(){
+
+//     const url = "http://localhost:3000/api/getdata-lib";
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     return { props: {data} };
+// }
---
+++ code/pages/class/markattendance.js
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+let classN = "Fall 2022 COSC 4319 Software Engineering";
+
+const Home_Faculty = ({ href, isSelected, title }) => (
+    <Link href={href}>
+      <a style={{padding: 5, margin: 5, backgroundColor: isSelected ? "blue" : "transparent",}}>
+        {title}
+</a>
+    </Link>
+)
+
+export default function Home() {
+  const { query } = useRouter();
+  const router = useRouter();
+
+  const isTabOneSelected = !!query.tabOne;
+  const isTabTwoSelected = !!query.tabTwo;
+  const isTabThreeSelected = !!query.tabThree;
+
+  const [data, setData] = useState([]);
+  const tablename = "users";
+
+  const{
+    query: className,
+  } = router;
+  const cName = className; 
+
+  console.log(className.className)
+  
+
+
+  return (
+    
+    <div className="fullpage">
+            <div className="App_NameF">
+            <img src="https://www.shsu.edu/dept/marketing/logos/SHSU-RGB_Orange%20Box.png" alt="SHSU"></img> 
+                <h2>
+                    Kat Scan
+                </h2>
+            </div>
+            
+      <main className="box">
+         
+        
+        <nav>
+            <Home_Faculty href="Home_Faculty" title="Home" isSelected={isTabOneSelected} >
+            
+            </Home_Faculty> 
+                
+            <Home_Faculty href="Create_Course" title="Create Course" isSelected={isTabTwoSelected} >
+      
+            </Home_Faculty> 
+      
+            <Home_Faculty href="View_Report" title="View Report" isSelected={isTabThreeSelected} >
+                
+            </Home_Faculty>
+          
+        </nav>
+        <section>
+        </section>
+        <div>
+            {/* {cName} */}
+        </div>
+        
+      </main>
+    </div>
+  );
+}
