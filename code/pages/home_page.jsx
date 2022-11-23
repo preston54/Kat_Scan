dd064ad5f9acd0e95651d651c94ec5e7a26c3b58
preston54 <81720648+preston54@users.noreply.github.com>
2022-11-23 16:08:33 -0600

    home page

---
+++ code/pages/home_page.jsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+
+let classN = "Fall 2022 COSC 4319 Software Engineering";
+
+const Home_Faculty = ({ href, isSelected, title }) => (
+    <Link href={href}>
+      <a style={{padding: 5, margin: 5, backgroundColor: isSelected ? "blue" : "transparent",}}>
+        {title}
+</a>
+    </Link>
+)
+
+export default function Home() {
+  const { query } = useRouter();
+
+  const isTabOneSelected = !!query.tabOne;
+  const isTabTwoSelected = !!query.tabTwo;
+  const isTabThreeSelected = !!query.tabThree;
+
+  return (
+    
+    <div class="fullpage">
+<div class="App_Name">
+            <img src="https://www.shsu.edu/dept/marketing/logos/SHSU-RGB_Orange%20Box.png" alt="SHSU"></img> 
+                <h2>
+                    Kat Scan
+                </h2>
+            </div>
+            
+      <main className="box">
+         
+        
+        <nav>
+            <Home_Faculty href="Home_Faculty" title="Home" isSelected={isTabOneSelected} >
+            
+            </Home_Faculty> 
+                
+            <Home_Faculty href="Create_Course" title="Create Course" isSelected={isTabTwoSelected} >
+      
+            </Home_Faculty> 
+      
+            <Home_Faculty href="view_table" title="View Report" isSelected={isTabThreeSelected} >
+                
+            </Home_Faculty>
+          
+        </nav>
+        <section>
+        </section>
+        <div class="Ver_SH box QrTabClass">
+            <p align="">"classN"</p>
+            <button class = "QRButton" type="submit">Generate QR code</button>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
